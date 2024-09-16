7fd544a22d9e5a3d0ffe2fa1da5cdd53c61a1058
Jae-Nadine <jae.nadine@icloud.com>
2024-09-16 05:07:32 -0700

    FinalCommit
    Finally figured out needed throws/exception to make code work!!!

--- Unit13/src/main/java/com/coderscampus/Unit13/RecipeController.java
+++ Unit13/src/main/java/com/coderscampus/Unit13/RecipeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,27 +20,27 @@ public class RecipeController {
 	private RecipeService recipeService;
 	
 	@GetMapping("/all-recipes")
-	public List<Recipe> getAllRecipes() {
+	public List<Recipe> getAllRecipes() throws IOException {
 		return recipeService.parseRecipesFromCSV();
 	}
 	
 	@GetMapping("/gluten-free") 
-	public List<Recipe> getGlutenFreeRecipes() {
+	public List<Recipe> getGlutenFreeRecipes() throws IOException {
 		return recipeService.parseRecipesFromCSV().stream().filter(Recipe::getGlutenFree).collect(Collectors.toList());
 	}
 	
 	@GetMapping("/vegan")
-	public List<Recipe> getVeganRecipes() {
+	public List<Recipe> getVeganRecipes() throws IOException {
 		return recipeService.parseRecipesFromCSV().stream().filter(Recipe::getVegan).collect(Collectors.toList());
 	}
 	
 	@GetMapping("/vegan-and-gluten-free")
-	public List<Recipe> getVeganAndGlutenFreeRecipes() {
+	public List<Recipe> getVeganAndGlutenFreeRecipes() throws IOException {
 		return recipeService.parseRecipesFromCSV().stream().filter(recipe -> recipe.getGlutenFree() && recipe.getVegan()).collect(Collectors.toList());
 	}
 	
 	@GetMapping("/vegetarian")
-	public List<Recipe> getVegetarianRecipes() {
+	public List<Recipe> getVegetarianRecipes() throws IOException {
 		return recipeService.parseRecipesFromCSV().stream().filter(Recipe::getVegetarian).collect(Collectors.toList());
 	}
 	
--- Unit13/src/main/java/com/coderscampus/Unit13/services/RecipeService.java
+++ Unit13/src/main/java/com/coderscampus/Unit13/services/RecipeService.java
@@ -1,24 +1,26 @@
 package com.coderscampus.Unit13.services;
 
 import com.coderscampus.Unit13.models.Recipe;
+
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-@Service 
+@Service
 public class RecipeService {
-	
-	public List<Recipe> parseRecipesFromCSV() {
+
+	public List<Recipe> parseRecipesFromCSV() throws IOException {
 		List<Recipe> recipes = new ArrayList<>();
 		
-		try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("Recipe.txt");
+		try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("recipes.csv");
 				
 				BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
 			CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT.withFirstRecordAsHeader().withTrim().withQuote('"'))) {
@@ -26,42 +28,47 @@ public class RecipeService {
 				System.out.println("CSV Headers: " + csvParser.getHeaderMap().keySet());
 				
 			for (CSVRecord csvRecord : csvParser) {
-				
-				System.out.println("CSV Record: " + csvRecord.toString());
+				try {
+					
+					Integer cookingMinutes = parseInteger(csvRecord.get("Cooking Minutes").trim());
+					Boolean dairyFree = Boolean.parseBoolean(csvRecord.get("Dairy Free").trim());
+					Boolean glutenFree = Boolean.parseBoolean(csvRecord.get("Gluten Free").trim());
+					String instructions = csvRecord.get("Instructions").trim();
+					Double preparationMinutes = parseDouble(csvRecord.get("Preparation Minutes").trim());
+					Double pricePerServing = parseDouble(csvRecord.get("Price Per Serving").trim());
+					Integer readyInMinutes = parseInteger(csvRecord.get("Ready In Minutes").trim());
+					Integer servings = parseInteger(csvRecord.get("Servings").trim());
+					Double spoonacularScore = parseDouble(csvRecord.get("Spoonacular Score").trim());
+					String title = csvRecord.get("Title").trim();
+					Boolean vegan = Boolean.parseBoolean(csvRecord.get("Vegan").trim());
+					Boolean vegetarian = Boolean.parseBoolean(csvRecord.get("Vegetarian").trim());
 				
 				Recipe recipe = new Recipe(
-					Integer.parseInt(csvRecord.get("Cooking Minutes")),
-					Boolean.parseBoolean(csvRecord.get("Dairy Free")),
-					Boolean.parseBoolean(csvRecord.get("Gluten Free")),
-					csvRecord.get("Instructions"),
-					Double.parseDouble(csvRecord.get("Preparation Minutes")),
-					Double.parseDouble(csvRecord.get("Price Per Serving")),
-					Integer.parseInt(csvRecord.get("Ready In Minutes")),
-					Integer.parseInt(csvRecord.get("Servings")),
-					Double.parseDouble(csvRecord.get("Spoonacular Score")),
-					csvRecord.get("Title"),
-					Boolean.parseBoolean(csvRecord.get("Vegan")),
-					Boolean.parseBoolean(csvRecord.get("Vegetarian"))
-				);
+						cookingMinutes, dairyFree, glutenFree, instructions, preparationMinutes, pricePerServing, 
+						readyInMinutes, servings, spoonacularScore, title, vegan, vegetarian);
+				
 				recipes.add(recipe);
+			} catch (NumberFormatException e) {
+				System.out.println("Invalid number format in CSV: " + csvRecord.toString());
+			}}}
+		return recipes;
 			}
-			
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			return recipes;
+
+	private Integer parseInteger(String value) {
+		try {
+			return Integer.parseInt(value);
+		} catch (NumberFormatException e) {
+			return null;
+		}
 	}
 
+	private Double parseDouble(String value) {
+		try {
+			return Double.parseDouble(value);
+		} catch (NumberFormatException e) {
+			return null;
+		}
+	}
 }
 
-
-
-
-
 //
-
-
-
-
-
-
